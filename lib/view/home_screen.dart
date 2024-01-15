23fc2f132f9ddd841541a5ea36b8377c726fc39b
ankit kumar <ankitduvedi.03@gmail.com>
2024-01-16 01:30:34 +0530

    Profile screen reels

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:while_app/main.dart';
 import 'package:while_app/resources/components/message/apis.dart';
 import 'package:while_app/view/create_screen.dart';
 import 'package:while_app/view/feed_screen.dart';
@@ -14,100 +15,77 @@ class HomeScreen extends ConsumerStatefulWidget {
   ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  int _currentIndex = 0;
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
+class _HomeScreenState extends ConsumerState<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
   @override
   void initState() {
     // listUsersFollowers();
     APIs.getSelfInfo();
     super.initState();
+    _controller = TabController(length: 5, vsync: this, initialIndex: 0);
   }
-
-  final List<Widget> _screens = [
-    const FeedScreen(),
-    const CreateScreen(),
-    const ReelsScreen(),
-    const SocialScreen(),
-    const ProfileScreen()
-  ];
 
   @override
   Widget build(BuildContext context) {
     //final currentTheme = ref.watch(themeNotifierProvider);
 
     return Scaffold(
-      body: _screens[_currentIndex], // replace with _currentIndex
+      body: TabBarView(
+        controller: _controller,
+        children: const [
+          FeedScreen(),
+          CreateScreen(),
+          ReelsScreen(),
+          SocialScreen(),
+          ProfileScreen()
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        color: Colors.black,
-        height: 50,
-        // shape: const CircularNotchedRectangle(),
-        //color: currentTheme.primaryColor,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            IconButton(
-              onPressed: () {
-                _onTabTapped(0);
-              },
-              icon: const Icon(
-                Icons.home_outlined,
-                size: 30,
-                color: Colors.white,
+          padding: EdgeInsets.only(bottom: 2),
+          color: Colors.black,
+          height: 50,
+          // shape: const CircularNotchedRectangle(),
+          //color: currentTheme.primaryColor,
+          child: TabBar(
+            controller: _controller,
+            indicatorColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelColor: Colors.white,
+            tabs: const [
+              Tab(
+                icon: Icon(
+                  Icons.home,
+                  size: 30,
+                ),
               ),
-            ),
-            IconButton(
-              onPressed: () {
-                _onTabTapped(1);
-              },
-              icon: const Icon(
-                Icons.videocam_outlined,
-                size: 30,
-                color: Colors.white,
+              Tab(
+                icon: Icon(
+                  Icons.videocam_outlined,
+                  size: 30,
+                ),
               ),
-            ),
-            IconButton(
-                iconSize: 65,
-                onPressed: () {
-                  _onTabTapped(2);
-                },
-                icon: const Icon(
+              Tab(
+                icon: Icon(
                   Icons.slow_motion_video_outlined,
                   size: 30,
-                  color: Colors.white,
-                )),
-            IconButton(
-              onPressed: () {
-                _onTabTapped(3);
-              },
-              icon: const Icon(
-                Icons.message_outlined,
-                size: 30,
-                color: Colors.white,
+                ),
               ),
-            ),
-            IconButton(
-              onPressed: () {
-                _onTabTapped(4);
-              },
-              icon: const Icon(
-                Icons.account_circle_outlined,
-                size: 30,
-                color: Colors.white,
+              Tab(
+                icon: Icon(
+                  Icons.message_outlined,
+                  size: 30,
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+              Tab(
+                icon: Icon(
+                  Icons.account_circle_outlined,
+                  size: 30,
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
